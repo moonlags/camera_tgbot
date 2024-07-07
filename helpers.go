4cf46d9672092e38d25b9379ec31b62f8469c42c
moonlags <moonlags.me@proton.me>
2024-07-07 15:39:00 +0300

    phone_init run

--- helpers.go
+++ helpers.go
@@ -2,27 +2,26 @@ package main
 
 import (
 	"fmt"
-	"log"
 	"net/http"
 )
 
 func setZoom(zoom int) {
 	url := fmt.Sprintf("http://127.0.0.1:8080/ptz?zoom=%v", zoom)
 	if _, err := http.Get(url); err != nil {
-		log.Fatal("Failed to set camera zoom:", err)
+		fmt.Println("Failed to set zoom:", err)
 	}
 }
 
 func enableNightVision() {
 	url := "http://127.0.0.1:8080/settings/night_vision?set=on"
 	if _, err := http.Get(url); err != nil {
-		log.Fatal("Failed to enable night vision:", err)
+		fmt.Println("Failed to enable night vision:", err)
 	}
 }
 
 func disableNightVision() {
 	url := "http://127.0.0.1:8080/settings/night_vision?set=off"
 	if _, err := http.Get(url); err != nil {
-		log.Fatal("Failed to disable night vision:", err)
+		fmt.Println("Failed to disable night vision:", err)
 	}
 }
